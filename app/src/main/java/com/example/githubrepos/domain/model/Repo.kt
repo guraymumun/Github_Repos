0e7cca15aaf425fcd2fd36603226c59f2f6e3fbb
Gyuray Myumyun <Gyuray.Myumyun@digitall.com>
2021-09-07 14:49:33 +0300

    Data binding added to RepoRecyclerAdapter

--- app/src/main/java/com/example/githubrepos/domain/model/Repo.kt
+++ app/src/main/java/com/example/githubrepos/domain/model/Repo.kt
@@ -1,3 +1,14 @@
 package com.example.githubrepos.domain.model
 
-data class Repo(var id: Int, var name: String, var description: String, var owner: Owner)
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+data class Repo(var id: Int, var name: String, var description: String, var owner: Owner) {
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter("imageUrl")
+        fun loadImage(view: ImageView, url: String) = Glide.with(view.context).load(url).into(view)
+    }
+}
--- app/src/main/java/com/example/githubrepos/presentation/adapter/RepoRecyclerAdapter.kt
+++ app/src/main/java/com/example/githubrepos/presentation/adapter/RepoRecyclerAdapter.kt
@@ -1,17 +1,11 @@
 package com.example.githubrepos.presentation.adapter
 
 import android.content.Context
-import android.icu.number.NumberFormatter.with
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.annotation.NonNull
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.Glide.*
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
 import com.example.githubrepos.databinding.ItemRepoBinding
 import com.example.githubrepos.domain.model.Repo
 
@@ -33,9 +27,7 @@ class RepoRecyclerAdapter(
 
     inner class RepoViewHolder(private val binding: ItemRepoBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(repo: Repo) {
-            binding.name.text = repo.name
-            binding.description.text = repo.description
-            Glide.with(context).load(repo.owner.avatar).into(binding.avatar)
+            binding.repo = repo
         }
     }
 }
